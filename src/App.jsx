e7f356117c77e268790e23259beda9f9ae77464a
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2025-11-14 14:52:42 +0800

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>tic tac toe game</h1>
+
+      <p>Welcome to the Tic Tac Toe game!</p>
+
+      <div className="container">
+        <button className="box"></button>
+        <button className="box"></button>
+        <button className="box"></button>
+        <button className="box"></button>
+        <button className="box"></button>
+        <button className="box"></button>
+        <button className="box"></button>
+        <button className="box"></button>
+        <button className="box"></button>
+      </div>
+    </div>
+  );
+}
